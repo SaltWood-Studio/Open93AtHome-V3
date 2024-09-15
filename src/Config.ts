eb582e69952f8465bd4fa1d9796de8872417fe76
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-09-15 23:50:53 +0800

    refactor: 现在部分环境变量不再必须

--- src/Config.ts
+++ src/Config.ts
@@ -5,9 +5,9 @@ import { Utilities } from './Utilities.js'
 export class Config {
     public static instance: Config
 
-    public readonly githubOAuthClientId: string = env.get('GITHUB_OAUTH_CLIENT_ID').required().asString()
-    public readonly githubOAuthClientSecret: string = env.get('GITHUB_OAUTH_CLIENT_SECRET').required().asString()
-    public readonly githubOAuthCallbackUrl: string = env.get('GITHUB_OAUTH_CALLBACK_URL').required().asString()
+    public readonly githubOAuthClientId: string = env.get('GITHUB_OAUTH_CLIENT_ID').default("").asString()
+    public readonly githubOAuthClientSecret: string = env.get('GITHUB_OAUTH_CLIENT_SECRET').default("").asString()
+    public readonly githubOAuthCallbackUrl: string = env.get('GITHUB_OAUTH_CALLBACK_URL').default("").asString()
     public readonly githubUrl: string = env.get('GITHUB_URL').default("github.com").asString()
     public readonly githubApiUrl: string = env.get('GITHUB_API_URL').default("api.github.com").asString()
     public readonly statsDir: string = env.get('STATISTICS_DIRECTORY').default("./stats").asString()
