ad307266f6cf6123d604d373ac8f4d4d0480be19
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-09-21 21:14:41 +0800

    fix: JavaScript 弱类型我恨你

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -452,7 +452,7 @@ export class Utilities {
     public static bigIntToBooleans(value: bigint, size: number): boolean[] {
         const bits = [];
         for (let i = 0; i < size; i++) {
-            bits.push((value & (1n << BigInt(i))) !== 0n); // 检查第 i 位是否为 1
+            bits.push((BigInt(value) & (1n << BigInt(i))) !== 0n); // 检查第 i 位是否为 1
         }
         return bits;
     }
