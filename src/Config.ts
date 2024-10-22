9e908def10ad1b9d5cbaaa309406bc544a2db562
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-10-22 21:43:56 +0800

    Release 3.1.8-pre2

--- src/Config.ts
+++ src/Config.ts
@@ -37,7 +37,7 @@ export class Config {
     public readonly sourceIpHeader: string = env.get('SOURCE_IP_HEADER').default("x-real-ip").asString();
     public readonly debug: boolean = env.get('DEBUG').default("false").asBool();
 
-    public static readonly version: string = "3.1.8-pre1";
+    public static readonly version: string = "3.1.8-pre2";
 
     private constructor() { }
 
