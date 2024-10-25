13a87b89447f3d0dc34546a1bd4a70d7dc7f799c
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-10-25 23:17:36 +0800

    Release 3.1.8-patch6

--- src/Config.ts
+++ src/Config.ts
@@ -37,7 +37,7 @@ export class Config {
     public readonly debug: boolean = env.get('DEBUG').default("false").asBool();
     public readonly disableAccessLog: boolean = env.get('DISABLE_ACCESS_LOG').default("false").asBool();
 
-    public static readonly version: string = "3.1.8-patch5";
+    public static readonly version: string = "3.1.8-patch6";
 
     private constructor() { }
 
