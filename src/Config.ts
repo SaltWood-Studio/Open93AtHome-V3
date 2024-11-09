03fcd4ad23e60a6890211b9445493b047ec55068
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-11-09 23:09:26 +0800

    Release 3.2.0-patch7

--- src/Config.ts
+++ src/Config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import env from 'env-var'
 import { defaultInstance } from './RateLimiter.js'
 
-export const version = "3.2.0-patch6";
+export const version = "3.2.0-patch7";
 
 export class Config {
     public static instance: Config
