c7b6d29e9feef9f59ebff227884f35ea4184a81d
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2025-01-12 22:41:03 +0800

    feat: 支持 JSON 返回随机文件

--- src/routes/ApiOther.ts
+++ src/routes/ApiOther.ts
@@ -4,7 +4,23 @@ import { ApiFactory } from "./ApiFactory.js";
 export class ApiOther {
     public static register(inst: ApiFactory) {
         inst.app.get("/api/other/random_file", (req, res) => {
-            res.redirect(encodeURI(Utilities.getRandomElement(inst.files)?.path || ''));
+            const type = req.query.type ?? "redirect";
+            switch (type) {
+                case "302":
+                case "redirect":
+                    res.redirect(encodeURI(Utilities.getRandomElement(inst.files)?.path || ''));
+                    break;
+                case "json":
+                    const url = Utilities.getRandomElement(inst.files)?.path || '';
+                    res.json({
+                        url: url,
+                        encodedUrl: encodeURI(url)
+                    });
+                default:
+                    res.status(400).json({
+                        error: "Bad request"
+                    });
+            }
         });
     }
 }
